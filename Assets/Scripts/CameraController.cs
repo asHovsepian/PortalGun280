53141f18f55988940a78f1b0743b4bb4cf362bcd
asHovsepian <asHovsepian@users.noreply.github.com>
2023-05-09 11:53:21 -0700

    cinemachine

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,16 +8,30 @@ public class CameraController : MonoBehaviour
     //this stores the action map i created
     private PlayerInput controls;
     //mouse sens
-    private float mouseSensitivity = 100f;
+    private float mouseSensitivity = 10f;
     //direction for mouse
     private Vector2 mouseLook;
-    //mouse rotation
+    //mouse rotation variables
     private float xRotation = 0f;
+    private float yRotation = 0f;
     //stores the players transform
     public Transform playerbody;
 
     private void Awake()
     {
         controls = new PlayerInput();
+        controls.Enable();
+    }
+    private void Start()
+    {
+        //locks the cursor into the game when testing in unity
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void Update()
+    {
+        //Debug.Log(controls.Player.Look.ReadValue<Vector2>());
+        mouseLook = controls.Player.Look.ReadValue<Vector2>();
+        transform.Rotate(mouseLook.x * Vector3.up * mouseSensitivity * Time.deltaTime);
+        transform.Rotate(mouseLook.y * Vector3.left * mouseSensitivity * Time.deltaTime);
     }
 }
